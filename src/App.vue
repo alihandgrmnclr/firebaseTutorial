449825dd66b04a2c14528b98d7bf4dc8d8c97b18
alihandgrmnclr <alihandegirmenciler@gmail.com>
2024-12-09 23:22:54 +0300

    feature: Export db, Get Data from DB

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
 <div class="app">
-  Our App
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <p>User Name: {{ user.name }}</p>
+        <p>User Age: {{ user.age }}</p>
+        <p>Is Active: {{ user.isActive }}</p>
+      </li>
+    </ul>
+  </div>
 </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+//
+import db from '../firebase/config'
+//
+import { onSnapshot, collection, getDocs,   } from 'firebase/firestore'
+
+const users = ref([])
+
+
+onMounted(async () => {
+  // Fetch data
+  // const querySnapshot = await getDocs(collection(db, "users"))
+  // users.value = querySnapshot.docs.map(doc => doc.data())
+  // console.log("Users:", users.value)
+
+  // Realtime data
+  onSnapshot(collection(db, "users"), (querySnapshot) => {
+    users.value = querySnapshot.docs.map(doc => doc.data())
+    console.log("Users:", users.value)
+  })
+})
 
 </script>
 
